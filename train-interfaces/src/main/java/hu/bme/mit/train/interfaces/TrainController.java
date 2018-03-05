329cde81086e6cbcdb60a56898e28d142349efdd
meres <marcsika96@gmail.com>
2018-03-05 06:29:50 -0800

    Problem solved in branch

--- train-interfaces/src/main/java/hu/bme/mit/train/interfaces/TrainController.java
+++ train-interfaces/src/main/java/hu/bme/mit/train/interfaces/TrainController.java
@@ -5,9 +5,13 @@ public interface TrainController {
 	void followSpeed();
 
 	int getReferenceSpeed();
+	
+	void setReferenceSpeed( int joystickPosition);
 
 	void setSpeedLimit(int speedLimit);
 
 	void setJoystickPosition(int joystickPosition);
+	
+	int getJoystickPosition();
 
 }
